1451c821e715f5e08ee746082e6a6dc11116a53c
evgenyalesich <quasimodo20042001@gmail.com>
2024-09-20 03:43:45 +0300

    Initial commit

--- my_notes_app/app/config.py
+++ my_notes_app/app/config.py
@@ -1,16 +1,21 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DB_USER: str = "DB_USER"
-    DB_PASSWORD: str = "DB_PASSWORD"
- 
-    DB_HOST: str = "DB_HOST"   # Default to localhost
-    DB_PORT: int = "DB_PORT"         # Default PostgreSQL port
-    DB_NAME: str = "DB_NAME"
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str = "localhost"  # Default to localhost
+    DB_PORT: int = 5432         # Default PostgreSQL port
+    DB_NAME: str
 
     class Config:
         env_file = ".env"  # Load from .env file
 
 settings = Settings()
 
-SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}"
+    f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+)
+print(settings.DB_PORT)  # Проверка типа данных
+print(type(settings.DB_PORT))  # Это должно вывести <class 'int'>
